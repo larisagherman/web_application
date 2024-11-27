dd9699e4707f0733a5ab7e80d2bdb2650811aad6
Gherman Larisa <gherman.larisa3@gmail.com>
2024-11-27 15:05:39 +0200

    header footer

---
+++ src/Footer.jsx
@@ -0,0 +1,8 @@
+function Footer() {
+    return (
+        <footer>
+            <p>&copy; F1 Schedule Predictions</p>
+       </footer> 
+    );
+}
+export default Footer;
---
+++ src/Header.jsx
@@ -0,0 +1,18 @@
+function Header() {
+    return (
+        <header>
+            <h1> F1 Schedule Predictions</h1>
+            <nav>
+                <u>
+                    <li><a href="#"></a>Home</li>
+                    <li><a href="#"></a>About</li>
+                    <li><a href="#"></a>Services</li>
+                    <li><a href="#"></a>Contact</li>
+
+                </u>
+            </nav>
+        <hr></hr>
+        </header>
+    );
+}
+export default Header;
